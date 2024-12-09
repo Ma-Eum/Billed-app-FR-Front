7f5933cd10a70d34a2003e3faafc41fe94635d93
Ma-Eum <cl.gachelin@gmail.com>
2024-12-09 19:52:56 +0100

    mise a jour

--- setup-jest.js
+++ setup-jest.js
@@ -1,2 +1,9 @@
 import $ from 'jquery';
+
+// Configure jQuery globalement pour les tests
 global.$ = global.jQuery = $;
+
+// Mock de la méthode `.modal` utilisée dans vos tests
+$.fn.modal = jest.fn(() => {
+  console.log("Mock modal triggered");
+});
